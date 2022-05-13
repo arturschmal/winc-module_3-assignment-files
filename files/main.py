1c76243885a8ca1579d932f83875c687de77ae56
Artur Schmal <artur@originaltype.com>
2022-05-13 10:10:46 +0200

    commit

--- files/main.py
+++ files/main.py
@@ -4,40 +4,48 @@ __human_name__ = "files"
 import os
 from zipfile import ZipFile
 
-# def clean_cache():
-#     path = 'files/cache'
-#     if os.path.isdir(path):
-#         for file_name in os.listdir(path):
-#             os.remove(path + '/' + file_name)
-#     else:
-#         os.mkdir(path)
+cache_folder = os.path.join('files', 'cache')
+PATH = os.path.join(os.getcwd(), cache_folder)
 
-# clean_cache()
+def clean_cache():
+    if os.path.isdir(PATH):
+        for file_name in os.listdir(PATH):
+            os.remove(os.path.join(PATH, file_name))
+    else:
+        os.mkdir(PATH)
+
 
 def cache_zip(zipfile_path, cache_dir):
     with ZipFile(zipfile_path) as myZip:
         myZip.extractall(cache_dir, members=None, pwd=None)
 
-# cache_zip('files/data.zip', 'files/cache')
-
-# def cached_files():
-#     files = []
-#     for filename in os.listdir('files/cache'):
-#         file_path = os.path.abspath(filename)
-#         if not filename in files:
-#                 files.append(file_path)
-#     return files
-
-# print(cached_files())
 
 def cached_files():
-    path = 'files/cache'
     files = []
-    for file in os.listdir(path):
-            if os.path.isfile(os.path.join(path, file)):
-                file_path = os.path.abspath(file)
+    for file in os.listdir(PATH):
+            file_path = os.path.join(PATH, file)
+            if os.path.isfile(file_path):
                 if not file_path in files:
                     files.append(file_path)
     return files
 
-print(cached_files())
+
+def find_password(filelist):
+    string = 'password'
+    for file in filelist:
+        open_file = open(file, "r")
+        lines = open_file.readlines()
+        for line in lines:
+            if string in line:
+                open(file, "r").close()
+                return line[line.find(" ")+1:-1]
+
+
+if __name__ == "__main__":
+    clean_cache()
+    cache_zip('files/data.zip', PATH)
+    print(cached_files()) 
+    print(find_password(cached_files()))
+
+
+
